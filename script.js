97f8f7cbbc7bf0c3f94ba5bb9899a97e42e78e33
cherukupally kalyan <88718253+kalyan-81@users.noreply.github.com>
2022-05-26 11:52:31 +0530

    updated script.js

--- script.js
+++ script.js
@@ -31,13 +31,13 @@ $(document).ready(function(){
     });
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Front-end Developer", "Python developer"],
+        strings: ["Front-end Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
     var typed = new Typed(".typing-2", {
-        strings: ["Front-end Developer", "Python developer"],
+        strings: ["Front-end Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
